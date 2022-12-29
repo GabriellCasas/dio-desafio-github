a3878af97c217dab7b6f0948ddf4df3a9531eb80
GabriellCasas <gabrielcasas527@gmail.com>
2022-12-29 17:31:07 -0300

    adição pasta ExemploFundamentos

---
+++ ExemploFundamentos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploFundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
